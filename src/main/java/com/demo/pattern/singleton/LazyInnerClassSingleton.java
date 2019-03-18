ddf3326d9ae3e2d82a29174935881b964b595c19
frankzhu520 <578640213@qq.com>
2019-03-18 22:36:27 +0800

    update

---
+++ src/main/java/com/demo/pattern/singleton/LazyInnerClassSingleton.java
@@ -0,0 +1,18 @@
+package com.demo.pattern.singleton;
+
+public class LazyInnerClassSingleton {
+
+    private LazyInnerClassSingleton(){
+        if(LazyHolder.LAZY != null){
+            throw new RuntimeException("不允许创建多个实例");
+        }
+    }
+
+    public static final LazyInnerClassSingleton getInstance(){
+        return LazyHolder.LAZY;
+    }
+
+    private static class LazyHolder{
+        private static final LazyInnerClassSingleton LAZY = new LazyInnerClassSingleton();
+    }
+}
